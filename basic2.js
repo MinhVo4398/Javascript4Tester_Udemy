d65c1365736d30e47c9d2f4b62ccb7734a77a5e0
MinhVo <minhvo4398@gmail.com>
2023-09-22 22:33:32 +0700

    while - do while

--- basic2.js
+++ basic2.js
@@ -1,10 +1,25 @@
 const flag = true
 
-if(flag)
+if(!flag)
 {   
     console.log("Condition satisfied")
 
 }
 else {
+    console.log(flag)
     console.log("condition NOT satisfied")
 }
+
+let i =0
+while(i>10)
+{
+    i++;
+    console.log(i)
+}
+
+do{
+    i++;
+    
+}while(i>10)
+console.log(i)
+
